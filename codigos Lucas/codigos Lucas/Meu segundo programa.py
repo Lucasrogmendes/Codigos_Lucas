653181e38da8414f6b5d22455782e5c9ee68d0ab
Lucas Rogerio <mendeslucasroger@gmail.com>
2025-09-04 16:29:30 -0300

    Add files via upload

---
+++ codigos Lucas/codigos Lucas/Meu segundo programa.py
@@ -0,0 +1,38 @@
+#exemplo de variavel: do tipo texto
+fruta = "manga" 
+
+print (type (fruta))
+#Exemplo Variavel do tipo INT é inteiro, números inteiros 
+idade = 35
+#Exemplo dp número quebrado,float
+note = float(9.5)
+
+# booleana 
+vc_curte = False
+
+### EXERCICIO 1 
+
+# Criar uma variavel que peça o nome do usuario pronto
+# depois criar uma variavel do tipo float, que pede a nota do usuario 
+# depois criar uma variavel que peça a idade do usuario 
+# depois de um print nas suas variaveis 
+
+usuario = input("digite o seu nome  : ")
+note = float(input("digit sua nota  : "))
+idade = int(input("Qual a sua Idade : "))
+print(usuario)
+print(note)
+print(idade)
+
+### exercicio 2
+ # crie uma variavel que se chama nome_carro,coloque dentro desse variavel o nome do seu carro dos sonhos 
+ # crie uma variavel que se chama preço, coloque dentro desssa variavel o preço do carro 
+ # crie uma variavel que se chama ,ano coloque dentro dessa variavel ano do carro
+
+nome_carro = ("dodge demon")
+preço = ("800 mil")
+ano = ("2018")
+
+print(nome_carro)
+print(preço)
+print(ano)
---
+++ codigos Lucas/codigos Lucas/Untitled-1.py
@@ -0,0 +1,7 @@
+nome_carro = ("dodge demon")
+preço = ("800 mil")
+ano = ("2018")
+
+print(nome_carro)
+print(preço)
+print(ano)
